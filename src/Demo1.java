3c0fab2f7bfbca2ffc3985b8da3542f43b267bb8
adankhara <adnakhara@live.co.uk>
2017-03-20 21:59:50 +0000

    Fixed project dependencies

--- src/Demo1.java
+++ src/Demo1.java
@@ -6,14 +6,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by Jagdish on 18/03/2017.
- */
 public class Demo1 {
-    public static void main(String[] args){
 
-    NopCommerceRegistration nopCommerce = new NopCommerceRegistration();
-    nopCommerce.Register();
-
+    public static void main(String[] args)
+    {
+        NopCommerceRegistration nopCommerce = new NopCommerceRegistration();
+        nopCommerce.Register();
     }
 }
